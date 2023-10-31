e0955d29c85eaa52aa0d9febebade70b1c82c3f4
Mario Fernandez <diddleslip@gmail.com>
2023-10-31 18:19:57 -0400

    That was a learning experience to say the least

--- MIT 6.00.2x.py
+++ MIT 6.00.2x.py
@@ -1,67 +1,67 @@
-## MIT 6.00.2x Code below:
+# ## MIT 6.00.2x Code below:
 
-# ---
+# # ---
 
-# print("Hello world!")
+# # print("Hello world!")
 
-#  ---
-
-NUMBER = 3
-def look_for_things(myList):
-    """Looks at all elements"""
-    for n in myList:
-      if n == NUMBER:
-        return True
-    return False
-
-# ---
+# #  ---
 
 # NUMBER = 3
-# def look_for_other_things(myList):
-#   """Looks at all pairs of elements"""
-#   for n in myList:
-#     for m in myList:
-#       if n - m == NUMBER or m - n == NUMBER:
+# def look_for_things(myList):
+#     """Looks at all elements"""
+#     for n in myList:
+#       if n == NUMBER:
 #         return True
-#   return False
+#     return False
 
-#  ---
+# # ---
 
-# def get_all_subsets(some_list):
-#   """Returns all subsets of size 0 - len(some_list) for some_list"""
-#   if len(some_list) == 0:
-#     # If the list is empty, return the empty list
-#     return [[]]
-#   subsets = []
-#   first_elt = some_list[0]
-#   rest_list = some_list[1:]
-#   # Strategy: Get all the subsets of rest_list; for each
-#   # of those subsets, a full subset list will contain both
-#   # the original subset as well as a version of the subset
-#   # that contains first_elt
-#   for partial_subset in get_all_subsets(rest_list):
-#     subsets.append(partial_subset)
-#     next_subset = partial_subset[:] + [first_elt]
-#     subsets.append(next_subset)
-#   return subsets
+# # NUMBER = 3
+# # def look_for_other_things(myList):
+# #   """Looks at all pairs of elements"""
+# #   for n in myList:
+# #     for m in myList:
+# #       if n - m == NUMBER or m - n == NUMBER:
+# #         return True
+# #   return False
 
-# NUMBER = 3
-# def look_for_all_the_things(myList):
-#   """Looks at all subsets of this list"""
-#   # Make subsets
-#   all_subsets = get_all_subsets(myList)
-#   for subset in all_subsets:
-#     if sum(subset) == NUMBER:
-#       return True
-#   return False
+# #  ---
 
-# print(look_for_all_the_things([1,2,4]))
+# # def get_all_subsets(some_list):
+# #   """Returns all subsets of size 0 - len(some_list) for some_list"""
+# #   if len(some_list) == 0:
+# #     # If the list is empty, return the empty list
+# #     return [[]]
+# #   subsets = []
+# #   first_elt = some_list[0]
+# #   rest_list = some_list[1:]
+# #   # Strategy: Get all the subsets of rest_list; for each
+# #   # of those subsets, a full subset list will contain both
+# #   # the original subset as well as a version of the subset
+# #   # that contains first_elt
+# #   for partial_subset in get_all_subsets(rest_list):
+# #     subsets.append(partial_subset)
+# #     next_subset = partial_subset[:] + [first_elt]
+# #     subsets.append(next_subset)
+# #   return subsets
 
-# ---
+# # NUMBER = 3
+# # def look_for_all_the_things(myList):
+# #   """Looks at all subsets of this list"""
+# #   # Make subsets
+# #   all_subsets = get_all_subsets(myList)
+# #   for subset in all_subsets:
+# #     if sum(subset) == NUMBER:
+# #       return True
+# #   return False
 
-# print(1%2)
+# # print(look_for_all_the_things([1,2,4]))
 
-# ---
+# # ---
+
+# # print(1%2)
+
+# # ---
 
 # generate all combinations of N items
 def powerSet(items):
@@ -70,38 +70,58 @@ def powerSet(items):
   for i in range(2**N):
     combo = []
     for j in range(N):
+      # print("this is i and j:", bin(i), j)
       # test bit jth of integer i
       if (i >> j) % 2 == 1:
         combo.append(items[j])
-      # print(bin(i), i, j, i >> j)
+        print(i, j, "THIS ONE DID IT")
+      print(i, j)
     yield combo
 
+def decimal_to_ternary(decimal, length):
+  ## THIS FUNCTION CONVERTS DECIMAL DIGITS INTO TERNARY DIGITS
+  result = ''
+
+  if decimal == 0:
+    while len(result) != length:
+       result = "0" + result
+
+  while decimal > 0:
+    remainder = decimal % 3
+    result = str(remainder) + result
+    decimal = decimal // 3
+
+  if length == len(result):
+    return result
+  else:
+    while len(result) != length:
+      result = "0" + result
+
+    return result
 
 def yieldAllCombos(items):
   """
     Generates all combinations of N items into two bags, whereby each
     item is in one or zero bags.
 
-    Yields a tuple, (bag1, bag2), where each bag is represented as
-    a list of which item(s) are in each bag.
+    Yields a tuple, (bag1, bag2), where each bag is represented as a list
+    of which item(s) are in each bag.
   """
   N = len(items)
 
   for i in range(3**N):
+    i = decimal_to_ternary(i, N)
     combo = ([], [])
-    for j in range(N):
-      if (i >> j) % 2 == 1:
-        # It goes in first bag
-        combo[0].append(items[j])
-      elif (i >> j) == 0:
-        pass
-      elif (i >> j) % 2 == 0:
-        # print("hello world!")
-        combo[1].append(items[j])
+
+    for index, j in enumerate(i):
+      if int(j) == 1:
+        combo[0].append(items[index])
+      elif int(j) == 2:
+        combo[1].append(items[index])
+
     yield combo
 
-for i in powerSet(["a", "b"]):
-  print(i)
+items = ['a', 'b', 'c', 'd', 'e', 'f']
 
-# for i in yieldAllCombos(["a", "b"]):
-#   print(i)
+for index, i in enumerate(yieldAllCombos(items)):
+  print(i)
