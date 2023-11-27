226099780afc368d10d11d064c1fa4016c24697d
Mario Fernandez <diddleslip@gmail.com>
2023-11-27 10:51:02 -0500

    Test

--- MIT 6.00.2x.py
+++ MIT 6.00.2x.py
@@ -279,21 +279,25 @@
 
 #  ---
 
-import numpy
+# import numpy
 
 
-def loadFile():
-  inFile = open('./julytemps.txt')
-  high = []
-  low = []
-  for line in inFile:
-    fields = line.split()
-    if len(fields) < 3 or not fields[0].isdigit():
-      continue
-    else:
-      high.append(int(fields[1]))
-      low.append(int(fields[2]))
-  diffTemps = list(numpy.array(high) - numpy.array(low))
-  return (diffTemps)
+# def loadFile():
+#   inFile = open('./julytemps.txt')
+#   high = []
+#   low = []
+#   for line in inFile:
+#     fields = line.split()
+#     if len(fields) < 3 or not fields[0].isdigit():
+#       continue
+#     else:
+#       high.append(int(fields[1]))
+#       low.append(int(fields[2]))
+#   diffTemps = list(numpy.array(high) - numpy.array(low))
+#   return (diffTemps)
 
-print(loadFile())
+# print(loadFile())
+
+#  ---
+
+
