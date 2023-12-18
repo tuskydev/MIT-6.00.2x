ef40f259b30d90ca1f56879857780b997c01f3e1
Mario Fernandez <diddleslip@gmail.com>
2023-12-18 18:32:41 -0500

    finished makeHistogram function

--- week6 Final/finals.py
+++ week6 Final/finals.py
@@ -93,47 +93,91 @@ import random, pylab
 
 # You are given this function
 def getMeanAndStd(X):
-    mean = sum(X)/float(len(X))
-    tot = 0.0
-    for x in X:
-        tot += (x - mean)**2
-    std = (tot/len(X))**0.5
-    return mean, std
+  mean = sum(X)/float(len(X))
+  tot = 0.0
+  for x in X:
+    tot += (x - mean)**2
+  std = (tot/len(X))**0.5
+  return mean, std
 
 # You are given this class
 class Die(object):
-    def __init__(self, valList):
-        """ valList is not empty """
-        self.possibleVals = valList[:]
-    def roll(self):
-        return random.choice(self.possibleVals)
+  def __init__(self, valList):
+    """ valList is not empty """
+    self.possibleVals = valList[:]
+  def roll(self):
+    return random.choice(self.possibleVals)
 
 # Implement this -- Coding Part 1 of 2
 def makeHistogram(values, numBins, xLabel, yLabel, title=None):
-    """
-      - values, a sequence of numbers
-      - numBins, a positive int
-      - xLabel, yLabel, title, are strings
-      - Produces a histogram of values with numBins bins and the indicated labels
-        for the x and y axis
-      - If title is provided by caller, puts that title on the figure and otherwise
-        does not title the figure
-    """
-    # TODO
+  """
+  - values, a sequence of numbers
+  - numBins, a positive int
+  - xLabel, yLabel, title, are strings
+  - Produces a histogram of values with numBins bins and the indicated labels
+    for the x and y axis
+  - If title is provided by caller, puts that title on the figure and otherwise
+    does not title the figure
+  """
+  if title:
+    pylab.title(title)
+
+  pylab.hist(values, numBins)
+  pylab.xlabel(xLabel)
+  pylab.ylabel(yLabel)
+  pylab.show()
+
+# Personal test case
+# makeHistogram([1,2], 4, "Aaa", "Bbb")
 
 # Implement this -- Coding Part 2 of 2
 def getAverage(die, numRolls, numTrials):
-    """
-      - die, a Die
-      - numRolls, numTrials, are positive ints
-      - Calculates the expected mean value of the longest run of a number
-        over numTrials runs of numRolls rolls
-      - Calls makeHistogram to produce a histogram of the longest runs for all
-        the trials. There should be 10 bins in the histogram
-      - Choose appropriate labels for the x and y axes.
-      - Returns the mean calculated
-    """
-    # TODO
+  """
+  - die, a Die
+  - numRolls, numTrials, are positive ints
+  - Calculates the expected mean value of the longest run of a number
+    over numTrials runs of numRolls rolls
+  - Calls makeHistogram to produce a histogram of the longest runs for all
+    the trials. There should be 10 bins in the histogram
+  - Choose appropriate labels for the x and y axes.
+  - Returns the mean calculated
+  """
+  results = []
+  for _ in range(numTrials):
+    currTrial = []
+    longestCurrStreak = []
+    highestStreak = 0
+
+    # Rolling dice for one trial
+    for _ in range(numRolls):
+      currTrial.append(die.roll())
+
+    # Indexing longest streak
+    for index, roll in enumerate(currTrial):
+      if len(longestCurrStreak) == 0:
+        longestCurrStreak.append(roll)
+        longestCurrStreak.append(1)
+        if len(currTrial) == 1:
+          if highestStreak < longestCurrStreak[1]:
+            highestStreak = longestCurrStreak[1]
+        continue
+      # Same roll in a row
+      if longestCurrStreak[0] == roll:
+        longestCurrStreak[1] += 1
+      # New roll, and check if the longestCurrStreak is higher than highestStreak
+      else:
+        if highestStreak < longestCurrStreak[1]:
+          highestStreak = longestCurrStreak[1]
+        longestCurrStreak[0] = roll
+        longestCurrStreak[1] = 1
+
+    results.append(highestStreak)
+
+  makeHistogram(results, 10, "Length of Longest Run", "Frequency", "Highest streak for dice roll experimentation")
+
+  return sum(results)/float(len(results))
+
 
 # One test case
-print(getAverage(Die([1,2,3,4,5,6,6,6,7]), 500, 10000))
+# print(getAverage(Die([1,2,3,4,5,6,6,6,7]), 500, 10000))
+print(getAverage(Die([1,2,3,4,5,6,6,6,7]), 1, 1000))
