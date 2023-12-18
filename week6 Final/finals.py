a835f63edd6629d31339e3903647e53d85fc6c6a
Mario Fernandez <diddleslip@gmail.com>
2023-12-18 15:30:28 -0500

    Adjusted tab spacing

--- week6 Final/finals.py
+++ week6 Final/finals.py
@@ -24,38 +24,38 @@ import random
 
 
 class Lecture(object):
-    def __init__(self, listen, sleep, fb):
-        self.listen = listen
-        self.sleep = sleep
-        self.fb = fb
-    def get_listen_prob(self):
-        return self.listen
-    def get_sleep_prob(self):
-        return self.sleep
-    def get_fb_prob(self):
-        return self.fb
+  def __init__(self, listen, sleep, fb):
+    self.listen = listen
+    self.sleep = sleep
+    self.fb = fb
+  def get_listen_prob(self):
+    return self.listen
+  def get_sleep_prob(self):
+    return self.sleep
+  def get_fb_prob(self):
+    return self.fb
 
 def get_mean_and_std(X):
-    mean = sum(X)/float(len(X))
-    tot = 0.0
-    for x in X:
-        tot += (x - mean)**2
-    std = (tot/len(X))**0.5
-    return mean, std
+  mean = sum(X)/float(len(X))
+  tot = 0.0
+  for x in X:
+    tot += (x - mean)**2
+  std = (tot/len(X))**0.5
+  return mean, std
 
 def lecture_activities(N, aLecture):
-    '''
-    N: integer, number of trials to run
-    aLecture: Lecture object
+  '''
+  N: integer, number of trials to run
+  aLecture: Lecture object
 
-    Runs a Monte Carlo simulation N times.
-    Returns: a tuple, (float, float)
-             Where the first float represents the mean number of lectures it takes 
-             to have a lecture in which all 3 activities take place,
-             And the second float represents the total width of the 95% confidence 
-             interval around that mean.
-    '''
-    # IMPLEMENT THIS FUNCTION
+  Runs a Monte Carlo simulation N times.
+  Returns: a tuple, (float, float)
+            Where the first float represents the mean number of lectures it takes 
+            to have a lecture in which all 3 activities take place,
+            And the second float represents the total width of the 95% confidence 
+            interval around that mean.
+  '''
+  # IMPLEMENT THIS FUNCTION
 
 
 # sample test cases
